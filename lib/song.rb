67b29d5977d09818ab52a713d12fd78158039b99
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-15 01:57:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name,genre,artist)
+  def initialize(name,artist,genre)
     @name = name
     @genre = genre
     @artist = artist
